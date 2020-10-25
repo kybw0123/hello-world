e077a4f90b4174494162aad1965b0dd5bf654e81
YongBae Kim <>
2020-10-25 11:13:20 +0900

    hello-world.py 생성
    간단한 git 명령어 사용법을 요약

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg949\cocoartf1671\cocoasubrtf600
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset129 AppleSDGothicNeo-Regular;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 print("hello horld")\
+print ('clone : 
+\f1 \'bf\'f8\'b0\'dd
+\f0  
+\f1 \'c0\'fa\'c0\'e5\'bc\'d2
+\f0  
+\f1 \'ba\'b9\'bb\'e7
+\f0 ')\
+
+\f1 print('add : \'bd\'ba\'c5\'d7\'c0\'cc\'c1\'f6 \'bf\'b5\'bf\'aa\'bf\'a1 \'c0\'db\'be\'f7 \'c6\'c4\'c0\'cf \'c3\'df\'b0\'a1')\
+print('commt : \'bd\'ba\'c5\'d7\'c0\'cc\'c1\'f6\'bf\'a1 \'bf\'c3\'b6\'f3\'bf\'c2 \'c6\'c4\'c0\'cf\'b5\'e9\'c0\'bb \'b0\'a1\'c1\'f6\'b0\'ed \'b3\'bb \'c4\'c4\'c7\'bb\'c5\'cd\'bf\'a1 \'c0\'fa\'c0\'e5(\'bc\'bc\'c0\'cc\'ba\'ea\'bf\'cd \'b0\'b0\'b4\'d9.)\
+print('push : \'c4\'bf\'b9\'d4\'b5\'e9\'c0\'bb \'bf\'f8\'b0\'dd \'c0\'fa\'c0\'e5\'bc\'d2\'bf\'a1 \'be\'f7\'b7\'ce\'b5\'e5')}
